e4752491805cfd21f9731b3d08fe37cba6fd3b52
mhusendev <husenrgc.96@gmail.com>
2023-05-18 16:24:28 +0700

    update

--- vue_chat/src/plugins/router.js
+++ vue_chat/src/plugins/router.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
-    {path:'',name:'home',props: true, component: ()=> import('@/iews/Home.vue')},
+    {path:'',name:'home',props: true, component: ()=> import('@/views/Home.vue')},
     {path:'/room',name:'room', props: true,  meta: {
         auth: true
     }, component: ()=> import('@/views/Room.vue')}
